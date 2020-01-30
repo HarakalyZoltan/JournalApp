e5408c635b309a4d9b5beb5deed86c09ac6c32ee
Harakaly Zoltan <harakaly.zoltan.1@gmail.com>
2020-01-30 17:46:29 +0200

    Fixing RecyclerAdapter + update on post journal layout

--- app/src/main/java/com/hz/journalapp/ui/JournalRecyclerAdapter.java
+++ app/src/main/java/com/hz/journalapp/ui/JournalRecyclerAdapter.java
@@ -70,76 +70,6 @@ public class JournalRecyclerAdapter extends RecyclerView.Adapter<JournalRecycler
                 .fit()
                 .into(viewHolder.image);
         viewHolder.dateAdded.setText(timeAgo);
-
-        viewHolder.deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewHolder.collectionReference.get()
-                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                            @Override
-                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                                for ( QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
-                                    Journal currentJournal = snapshot.toObject(Journal.class);
-
-                                    if (currentJournal.getUserId().equals(journal.getUserId())
-                                            && currentJournal.getTimeAdded().equals(journal.getTimeAdded())) {
-                                        viewHolder.collectionReference.document(snapshot.getId()).delete();
-                                        notifyItemRemoved(position);
-                                        journalList.remove(position);
-                                    }
-                                }
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(context, "Failure", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            }
-        });
-        viewHolder.editButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-                BitmapDrawable bitmapDrawable = ((BitmapDrawable) viewHolder.image.getDrawable());
-                Bitmap bitmap = bitmapDrawable.getBitmap();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-                String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title", null);
-                Uri bitmapUri = Uri.parse(path);
-
-                final Intent intent = new Intent(context, EditJournalActivity.class);
-                intent.putExtra("username", journal.getUserName());
-                intent.putExtra("thought", journal.getThought());
-                intent.putExtra("title", journal.getTitle());
-                intent.putExtra("timeAdded", journal.getTimeAdded().toDate().toString());
-                intent.putExtra("image", bitmapUri.toString());
-
-                viewHolder.collectionReference.get()
-                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                            @Override
-                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                                for ( QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
-                                    Journal currentJournal = snapshot.toObject(Journal.class);
-
-                                    if (currentJournal.getUserId().equals(journal.getUserId())
-                                            && currentJournal.getTimeAdded().equals(journal.getTimeAdded())) {
-                                        viewHolder.documentId = snapshot.getId();
-                                        intent.putExtra("documentId", viewHolder.documentId);
-                                    }
-                                }
-                                context.startActivity(intent);
-                                ((AppCompatActivity)context).finish();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(context, "Failure", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            }
-        });
     }
 
     @Override
@@ -147,23 +77,23 @@ public class JournalRecyclerAdapter extends RecyclerView.Adapter<JournalRecycler
         return journalList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        public TextView
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView
                 title,
                 thoughts,
                 dateAdded,
                 name;
-        public ImageView image;
-        public ImageButton shareButton;
-        public ImageButton editButton;
-        public ImageButton deleteButton;
-        public String documentId;
+        private ImageView image;
+        private ImageButton shareButton;
+        private ImageButton editButton;
+        private ImageButton deleteButton;
+        private String documentId;
 
         //FireStore
         private FirebaseFirestore db = FirebaseFirestore.getInstance();
         private CollectionReference collectionReference = db.collection("Journal");
 
-        public ViewHolder(@NonNull final View itemView, final Context ctx) {
+        private ViewHolder(@NonNull final View itemView, final Context ctx) {
             super(itemView);
             context = ctx;
             title = itemView.findViewById(R.id.journal_title_list);
@@ -175,25 +105,109 @@ public class JournalRecyclerAdapter extends RecyclerView.Adapter<JournalRecycler
             editButton = itemView.findViewById(R.id.journal_row_edit_button);
             deleteButton = itemView.findViewById(R.id.journal_row_delete_button);
 
-            shareButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            shareButton.setOnClickListener(this);
+            editButton.setOnClickListener(this);
+            deleteButton.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.journal_row_share_button:
+                    shareJournal();
+                    break;
+                case R.id.journal_row_edit_button:
+                    editJournal();
+                    break;
+                case R.id.journal_row_delete_button:
+                    deleteThought();
+                    break;
+            }
+        }
+
+        private void deleteThought() {
+            final int position = getAdapterPosition();
+            final Journal journal = journalList.get(position);
+                    collectionReference.get()
+                            .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                @Override
+                                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                    for ( QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                        Journal currentJournal = snapshot.toObject(Journal.class);
+
+                                        if (currentJournal.getUserId().equals(journal.getUserId())
+                                                && currentJournal.getTimeAdded().equals(journal.getTimeAdded())) {
+                                            collectionReference.document(snapshot.getId()).delete();
+                                            notifyItemRemoved(position);
+                                            journalList.remove(position);
+                                        }
+                                    }
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(context, "Failure", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+        }
+
+        private void editJournal() {
+            final Journal journal = journalList.get(getAdapterPosition());
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
                     BitmapDrawable bitmapDrawable = ((BitmapDrawable) image.getDrawable());
                     Bitmap bitmap = bitmapDrawable.getBitmap();
                     bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
                     String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title", null);
                     Uri bitmapUri = Uri.parse(path);
 
-                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
-                    shareIntent.setType("image/jpeg");
-                    shareIntent.putExtra(Intent.EXTRA_STREAM, bitmapUri);
-                    shareIntent.setType("text/plain");
-                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, title.getText().toString().trim());
-                    shareIntent.putExtra(Intent.EXTRA_TEXT, thoughts.getText().toString().trim());
-                    context.startActivity(Intent.createChooser(shareIntent, "Share this!"));
-                }
-            });
+                    final Intent intent = new Intent(context, EditJournalActivity.class);
+                    intent.putExtra("username", journal.getUserName());
+                    intent.putExtra("thought", journal.getThought());
+                    intent.putExtra("title", journal.getTitle());
+                    intent.putExtra("timeAdded", journal.getTimeAdded().toDate().toString());
+                    intent.putExtra("image", bitmapUri.toString());
+
+                    collectionReference.get()
+                            .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                @Override
+                                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                    for ( QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                        Journal currentJournal = snapshot.toObject(Journal.class);
+
+                                        if (currentJournal.getUserId().equals(journal.getUserId())
+                                                && currentJournal.getTimeAdded().equals(journal.getTimeAdded())) {
+                                            documentId = snapshot.getId();
+                                            intent.putExtra("documentId", documentId);
+                                        }
+                                    }
+                                    context.startActivity(intent);
+                                    ((AppCompatActivity)context).finish();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(context, "Failure", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+        }
+
+        private void shareJournal() {
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            BitmapDrawable bitmapDrawable = ((BitmapDrawable) image.getDrawable());
+            Bitmap bitmap = bitmapDrawable.getBitmap();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+            String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title", null);
+            Uri bitmapUri = Uri.parse(path);
+
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("image/jpeg");
+            shareIntent.putExtra(Intent.EXTRA_STREAM, bitmapUri);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, title.getText().toString().trim());
+            shareIntent.putExtra(Intent.EXTRA_TEXT, thoughts.getText().toString().trim());
+            context.startActivity(Intent.createChooser(shareIntent, "Share this!"));
         }
     }
 }
